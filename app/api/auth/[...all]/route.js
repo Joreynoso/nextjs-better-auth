99ef15f73439025c6145c8049fba27dae7902138
Joreynoso <joreynoso.developer@gmail.com>
2025-11-23 20:18:09 -0300

    feat: Implement user registration with better-auth client and API routes.

--- app/api/auth/[...all]/route.js
+++ app/api/auth/[...all]/route.js
@@ -1,4 +1,5 @@
-import auth from '@/lib/auth'
-import { toNextJsHandler } from 'better-auth/next-js'
+import auth from "../../../../lib/auth"
 
-export const { POST, GET } = toNextJsHandler(auth)
+export const GET = auth.handler
+export const POST = auth.handler
+
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -5,26 +5,45 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
-import { signUp } from '@/lib/auth-client'
+import { authClient, signUp } from '@/lib/auth-client'
 
 export default function RegisterPage() {
     const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
     const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
-
 
     const router = useRouter()
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         setLoading(true)
+
+        if (password !== confirmPassword) {
+            setError('Las contraseñas no coinciden')
+            setLoading(false)
+            return
+        }
         try {
-            console.log('submitting form...')
-            await signUp({
+
+            const result = await authClient.signUp.email({
                 email,
                 password,
+                name,
+                callbackURL: '/dashboard'
             })
+
+            if (result.error) {
+                setError('Error al registrar')
+                console.log(error)
+                setLoading(false)
+                return
+            }
+
+            console.log(result)
+            router.push('/auth/login')
         } catch (error) {
             console.log(error)
             setError('Algo salio mal')
@@ -39,11 +58,43 @@ export default function RegisterPage() {
             <h2 className='text-2xl font-bold mb-6'>Registrate con <span className='text-blue-500'>better-auth</span></h2>
 
             <form onSubmit={handleSubmit} className='flex flex-col gap-4 w-full max-w-md bg-zinc-50 p-6 rounded-lg shadow-md'>
+
+                <Label htmlFor="name">Nombre</Label>
+                <Input
+                    type="text"
+                    placeholder="Nombre"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)} />
+
+                {/* email input */}
                 <Label htmlFor="email">Correo</Label>
-                <Input type="email" placeholder="Correo" />
+                <Input
+                    type="email"
+                    placeholder="Correo"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} />
+
+                {/* password input */}
                 <Label htmlFor="password">Contraseña</Label>
-                <Input type="password" placeholder="Contraseña" />
-                <Button type="submit">Registrarse</Button>
+                <Input
+                    type="password"
+                    placeholder="Contraseña"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} />
+
+                {/* confirm password input */}
+                <Label htmlFor="confirmPassword">Confirmar Contraseña</Label>
+                <Input
+                    type="password"
+                    placeholder="Confirmar Contraseña"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)} />
+
+                {/* button */}
+                <Button type="submit">{loading ? 'Registrando...' : 'Registrarse'}</Button>
+
+                {/* error section, must be an array*/}
+                {error && <p className='text-red-500'>{error}</p>}
             </form>
         </div>
     )
